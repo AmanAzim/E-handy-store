e032faf5644a372c5edfd25d0844a94e3668c091
Aman <rumman.djuice1@gmail.com>
2019-05-30 18:30:42 +0200

    commit

--- src/components/cart/PayPalButton.js
+++ src/components/cart/PayPalButton.js
@@ -10,20 +10,20 @@ class PayPalButton extends Component {
             this.props.clearCart();
             this.props.history.push('/');
             // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
-        }
+        };
 
         const onCancel = (data) => {
             // User pressed "cancel" or close Paypal's popup!
             console.log('The payment was cancelled!', data);
             // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
-        }
+        };
 
         const onError = (err) => {
             // The main Paypal's script cannot be loaded or somethings block the loading of that script!
             console.log("Error!", err);
             // Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
             // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
-        }
+        };
 
         let env = 'sandbox'; // you can set here to 'production' for production
         let currency = 'EUR'; // or you can set this value from your props or state
@@ -33,7 +33,7 @@ class PayPalButton extends Component {
         const client = {
             sandbox: process.env.REACT_APP_PAYPAL_ID,
             production: 'YOUR-PRODUCTION-APP-ID',
-        }
+        };
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
         // For sandbox app-ID (after logging into your developer account, please locate the "REST API apps" section, click "Create App"):
         //   => https://developer.paypal.com/docs/classic/lifecycle/sb_credentials/
