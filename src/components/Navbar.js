c1378ed229392d246e5b40540b613189d68eb830
Aman <rumman.djuice1@gmail.com>
2019-06-08 05:52:42 +0200

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {NavLink} from "react-router-dom";
-import logo from '../logo.svg'
+import logo from '../logo2.svg'
 import {ButtonContainer} from "./Button";
 import styled from 'styled-components';
 import '../App.css';
@@ -20,7 +20,7 @@ export default class Navbar extends Component {
         return (
           <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
 
-              <NavLink to="/" ><img src={logo} alt="E-Handy Shop" className="navbar-brand"/></NavLink>
+              <NavLink to="/" ><img src={logo} alt="E-Handy Shop" className="myLogo navbar-brand"/></NavLink>
 
               <ul className="navbar-nav align-items-center">
                   <li className="nav-item ml-5">
